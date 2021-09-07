e77cc6954f5e10909d25a96657274966bf6c0f60
Lucia Yu <53325538+LuciaFish@users.noreply.github.com>
2021-09-07 16:56:37 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+def sum_two(a,b):
+    '''
+    Arg:
+        a,b: two int
+    
+    Returns:
+        c: int that equals to the sum of two arguments
+    '''
+    c = a + b
+    return c
+
+def pdt_two(e,f):
+    '''
+    Arg:
+        e,f: two int
+    
+    Returns:
+        g: int that equals to the sum of two arguments
+    '''
+    g = e * f
+    return g
